bed7771e2ddc5b9aed3f6477a38362a37a15a7bf
simon <simon@klein-homepage.de>
2023-08-16 16:39:23 +0300

    corrected init code hash

--- projects/v3-periphery/contracts/libraries/PoolAddress.sol
+++ projects/v3-periphery/contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0x6ce8eb472fa82df5469c6ab6d485f17c3ad13c8cd7af59b3d4a8026c5ce0f7e2;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x4a368fd18f9a1f54d377052fbabd82aa768582b85d80f470018290f3364574a9;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
